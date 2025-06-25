ef6cf619feb527031ad20ee3d8e584441c0cb1c4
설화 <62371681+citrusfruits6276@users.noreply.github.com>
2025-06-25 21:52:27 +0900

    [Bronze V] Title: A+B, Time: 48 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program{
+    static void Main(String[] args){
+        string[] read = Console.ReadLine().Split(' ');
+        int A = int.Parse(read[0]);
+        int B = int.Parse(read[1]);
+        Console.WriteLine(A+B);
+    }
+}
