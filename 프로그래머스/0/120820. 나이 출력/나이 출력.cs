5e98eda4aafe36241cafa71c9d76bbf7ae1c51f6
설화 <62371681+citrusfruits6276@users.noreply.github.com>
2025-06-26 21:35:25 +0900

    [level 0] Title: 나이 출력, Time: 0.20 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        int years = 2022;
+        answer = years - age + 1;
+        return answer;
+    }
+}
